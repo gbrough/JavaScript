45187204d3ad633a7e841cd6579566e0b46a90e9
gbrough <gbrough@gmail.com>
2021-08-13 12:45:11 -0700

    complete solution

--- Python/Count Items Matching a Rule.py
+++ Python/Count Items Matching a Rule.py
@@ -1,4 +1,11 @@
 #Leetcode Count Items Matching a Rule
 
-
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        dict = {'type': 0, 'color': 1, 'name': 2}
+        count = 0
+        for item in items:
+            if item [dict[ruleKey]] == ruleValue:
+                count += 1
+        return count
 
